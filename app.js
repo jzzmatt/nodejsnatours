a6134e031a00aed254e0990b6456762add48cf66
jmateus <jmateus@jmateuss-iMac.local>
2020-06-19 19:48:17 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+
+app.use(express.json());
+/*
+INITIAL TEST
+app.get('/', (req, res) => {
+ res.status(200)
+     .json({message: 'hello from the server side!', app: 'Natours'});
+});
+
+app.post('/', (req, res) => {
+    res.send('You can post to this endpoint...');
+})*/
+
+//Initial Route
+const tours = JSON.parse(
+    fs.readFileSync('./dev-data/data/tours-simple.json')
+    );
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+           status: 'success',
+           results: tours.length,
+           data: {
+               tours
+           }
+        });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+ console.log(req.body);
+ res.send('Done')
+})
+//Start Server Express
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App runing on port ${port}...`);
+});
+
+
