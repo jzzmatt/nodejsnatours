3e3a2ae02467fa34dad2c54811d03663e488b74e
jmateus <jmateus@jmateuss-iMac.local>
2020-06-23 12:20:01 +0100

    refactoring

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,35 +1,9 @@
 //const fs = require('fs');
 const Tour = require('./../models/tourModel');
-const e = require('express');
-const { Query } = require('mongoose');
-const { listen } = require('../app');
+const APIFeatures = require('./../utils/apiFeatures');
 
 
-// const tours = JSON.parse(
-//     fs.readFileSync('./dev-data/data/tours-simple.json')
-//     );
-
-// exports.checkID = (req, res, next, val) => {
-//     console.log(`Tour id is: ${val}`);
-//     // if (req.params.id * 1 > tours.length) {
-//     //     return res.status(404).json({
-//     //         status: 'fail',
-//     //         message: 'Invalid ID'
-//     //     });
-//     // }
-//     next();
-// };
-
-// exports.checkBody = (req, res, next) => {
-//     if (!req.body.name || !req.body.price) {
-//         return res.status(400).json({
-//             status: 'fail',
-//             message: 'Missing name or price'
-//         })
-//     }
-//     next();
-// }
-
+//--0)
 //this middleware, permit to modify req on behalf of the user query
 exports.aliasTopTours = async (req, res, next) => {
     req.query.limit = '5';
@@ -38,90 +12,20 @@ exports.aliasTopTours = async (req, res, next) => {
     next();
 }
 
-// ---> REFACTORING <---
+
 
 exports.getAllTours = async (req, res) => {
 
     //Query all asset on the DB
     try {
-        //BUILD & Check for Query Request
-        //console.log(req.query)
-
-        //--1) Normal Filtering
-
-        const queryObj = {...req.query}
-        const excludedFields = ['page','sort','limit','fields'];
-        excludedFields.forEach(el => delete queryObj[el]);
-
-        //console.log(req.query, queryObj);
-
-
-        //Query DB Case 0
-
-        
-        
-        // Query CHAINING Prototype
-
-        //  //Query DB Case 1
-        // const tours = await Tour.find({
-        //     duration: 5,
-        //     difficulty: 'easy'
-        // });  
-        //Query DB Case 2
-        // const tours = await Tour.find()
-        //                         .where('duration')
-        //                         .equals(5)
-        //                         .where('difficulty')
-        //                         .equals('easy')
-
-        //--2) Advanced Filtering
-
-        // { difficulty : 'easy', duration: {$gte: 5}} //mongo format
-        // { difficulty: 'easy', duration: { gte: '5'}} // req.query format
-        //need to match [gte, gt, lte, lt] and change it to $gte, $gt, $lte which is mongo format
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-        let query = Tour.find(JSON.parse(queryStr));
-
-        //--3) Sorting
-        //console.log(req.query.sort)
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            //console.log(sortBy);
-            query = query.sort(sortBy);
-        } else {
-            query = query.sort('-createdAt');  //this will be as a default filtering , if the user doesn't specify
-        }
-
-        //--4) Field Limiting
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);  
-        }else {
-            query = query.select('-__v')
-        }
-
-        //--5) Pagination
-        
-        const page = req.query.page * 1 || 1;// or giv it the default value of 1
-        const limit = req.query.limit * 1 || 100; //giv it the default of 100
-        const skip = (page -1) * limit;
-
-        //page=2&limit=10, 1-10,page1; 11-20,page2 ; 21-30,page3
-        //query = query.skip(10).limit(10);
-
-        query = query.skip(skip).limit(limit);
-
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments(); //get the amount of document on db
-            if (skip >= numTours) throw new Error('This page does not Exist')
-        }
-    
-     
         //EXECUTE QUERY
-        //now we have query.sort().select().skip().limit()
-        const tours = await query;
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+
+        const tours = await features.query;
                                 
         // SEND RESPONSE
         res.status(200).json({
@@ -132,7 +36,7 @@ exports.getAllTours = async (req, res) => {
             }
         });  
 
-    }catch(err) {
+    } catch(err) {
        res.status(404).json({
            status: 'fail',
            message: err
---
+++ utils/apiFeatures.js
@@ -0,0 +1,62 @@
+class APIFeatures {
+    constructor(query, queryString) {
+        this.query = query //query object for Mongoo DB-> Tour.find()
+        this.queryString = queryString // req.query, query coming for the user request
+
+    }
+    filter() {
+        const queryObj = {...this.queryString}
+        const excludedFields = ['page','sort','limit','fields'];
+        excludedFields.forEach(el => delete queryObj[el]);
+
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        this.query = this.query.find(JSON.parse(queryStr));
+
+        return this //this will retur the entire object 
+    }
+    sort() {
+        if (this.queryString.sort) {
+            const sortBy = this.queryString.sort.split(',').join(' ');
+            //console.log(sortBy);
+            this.query = this.query.sort(sortBy);
+        } else {
+            this.query = this.query.sort('-createdAt');  //this will be as a default filtering , if the user doesn't specify
+        }
+
+        return this
+
+    }
+
+    limitFields() {
+
+        if (this.queryString.fields) {
+            const fields = this.queryString.fields.split(',').join(' ');
+            this.query = this.query.select(fields);  
+        }else {
+            this.query = this.query.select('-__v')
+        }
+
+        return this;
+
+    }
+
+    paginate() {
+
+        const page = this.queryString.page * 1 || 1;// or giv it the default value of 1
+        const limit = this.queryString.limit * 1 || 100; //giv it the default of 100
+        const skip = (page -1) * limit;
+
+        //page=2&limit=10, 1-10,page1; 11-20,page2 ; 21-30,page3
+        //query = query.skip(10).limit(10);
+
+        this.query = this.query.skip(skip).limit(limit);
+
+        return this;
+
+    }
+};
+
+
+module.exports = APIFeatures;
